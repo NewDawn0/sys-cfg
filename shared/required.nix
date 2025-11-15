734223fb20c3f89a8208a161c5eca3e4d3dbb20f
NewDawn0 <newdawn.v0.0+git@gmail.com>
2025-11-15 02:25:59 +0100

    Move users.nix -> required.nix and add systemStateversion
    Signed-off-by: NewDawn0 <newdawn.v0.0+git@gmail.com>

--- shared/required.nix
+++ shared/required.nix
@@ -1,10 +1,15 @@
-let
+{
+  fn,
+  lib,
+  ...
+}: let
   extraGroups = [
     "networkmanager"
     "wheel"
   ];
   hashedPassword = "$6$qZmB8JLWqy86gMdw$aciIbKbCygzoFcc1909fX47A9BCw7SL3MBpuNHSGO37.14ujd9nxCujtSYW0yAwfF/LXqMUXObr158Q5ZxPu2/";
 in {
+  system.stateVersion = fn.util.getVersion lib.version;
   users.users = {
     dawn = {
       inherit extraGroups hashedPassword;
@@ -12,6 +17,7 @@ in {
     };
     root = {
       inherit extraGroups hashedPassword;
+      isNormalUser = false;
     };
   };
 }
