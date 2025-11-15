27ec2ee70dd648ecf1eab156c7bebc9562ac5f78
NewDawn0 <newdawn.v0.0+git@gmail.com>
2025-11-15 22:50:30 +0100

    temporary secrets solution

--- hosts/schroedinger/disko.nix
+++ hosts/schroedinger/disko.nix
@@ -3,28 +3,35 @@
   pkgs,
   ...
 }: let
-  disk-cfg = {
-    dev1 = "/dev/nvme0n1";
-    luks = {
-      # main = "${pkgs.secrets}/secrets/luks/main.key";
-      # pass = "${pkgs.secrets}/secrets/luks/pass.txt";
-      main = "/secrets/luks/main.key";
-      pass = "/secrets/luks/pass.txt";
-    };
+  disk = "/dev/nvme0n1";
+  mkSecret = {
+    path,
+    outPath ? path,
+  }: let
+    pkg = pkgs.runCommandNoCC "luks-secret" {} ''
+      install -D ${path} $out/${outPath}
+    '';
+  in "${pkg}/${outPath}";
+  mainKey = mkSecret {
+    path = ../../secrets/luks-main.key;
+    outPath = "luks-main.key";
+  };
+  passKey = mkSecret {
+    path = ../../secrets/luks-pass.txt;
+    outPath = "luks-pass.txt";
   };
 in
   with fn; {
-    boot.initrd.secrets."/luks-main.key" = disk-cfg.luks.main;
+    boot.initrd.secrets."/luks-main.key" = mainKey;
     disko.devices.disk.main = fs.mkDisk {
-      disk = disk-cfg.dev1;
+      inherit disk;
       partitions = {
         esp = fs.mkESP {};
         swap = fs.mkSwap {size = "32G";};
         shared = fs.mkShared {size = "400G";};
         windows = fs.mkWindows {size = "600G";};
         root = fs.mkLuks {
-          mainKey = "/luks-main.key";
-          passKey = "/luks-pass.txt";
+          inherit mainKey passKey;
           content = fs.mkBtrfs {
             subvolumes = {
               "@root" = fs.mkSubvol "/";
