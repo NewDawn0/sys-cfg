867a4423a87986cc4b8c9b20b2f07ef7d0baf0ef
NewDawn0 <newdawn.v0.0+git@gmail.com>
2025-11-15 02:28:06 +0100

    Add automatic version extraction function
    Signed-off-by: NewDawn0 <newdawn.v0.0+git@gmail.com>

--- fn/util.nix
+++ fn/util.nix
@@ -1,13 +1,21 @@
-let
-  importRec =
-    base:
-    let
-      paths = with builtins; attrNames (readDir base);
-      filtered = builtins.filter (x: x != "default.nix");
-      toPath = x: base + "/${x}";
-    in
-    map (toPath) (filtered paths);
-in
 {
-  inherit importRec;
+  # Parse the current version from a full version string
+  # @param  str       full            - Full version string from lib.version:
+  # @return str       parsed          - Parsed version
+  getVersion = full: let
+    match = builtins.match "([0-9]+\.[0-9]+).*" full;
+  in
+    if match == [] || match == null
+    then throw "Unable to parse version from ${full}"
+    else builtins.head match;
+
+  # Import all items in a directory
+  # @param  path      base            - Base path to import from
+  # @return [path]    paths           - List of paths
+  importRec = base: let
+    paths = with builtins; attrNames (readDir base);
+    filtered = builtins.filter (x: x != "default.nix");
+    toPath = x: base + "/${x}";
+  in
+    map toPath (filtered paths);
 }
