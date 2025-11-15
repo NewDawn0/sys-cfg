e9efbb418e0ed24122fcc81bfba3902479f767ba
NewDawn0 <newdawn.v0.0+git@gmail.com>
2025-11-15 02:31:10 +0100

    Remove duplicates from the boot module
    Signed-off-by: NewDawn0 <newdawn.v0.0+git@gmail.com>

--- mod.nix
+++ mod.nix
@@ -4,30 +4,7 @@
   config,
   pkgs,
   ...
-}:
-{
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
+}: {
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
-
-  system.stateVersion = "25.05";
-
-  # Hardware config.nix
-  imports = [
-    (modulesPath + "/installer/scan/not-detected.nix")
-  ];
-
-  boot.initrd.availableKernelModules = [
-    "nvme"
-    "xhci_pci"
-    "sdhci_pci"
-  ];
-  boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-amd" ];
-  boot.extraModulePackages = [ ];
-
-  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
