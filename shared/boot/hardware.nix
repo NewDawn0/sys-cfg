5c93894fdc8632b1baa52cea3cd5ece1605328c1
NewDawn0 <newdawn.v0.0+git@gmail.com>
2025-11-15 02:29:50 +0100

    Add hardware boot setttings
    Signed-off-by: NewDawn0 <newdawn.v0.0+git@gmail.com>

---
+++ shared/boot/hardware.nix
@@ -0,0 +1,10 @@
+{
+  hardware = {
+    enableAllFirmware = true;
+    enableAllHardware = true;
+    cpu = {
+      amd.updateMicrocode = true;
+      intel.updateMicrocode = true;
+    };
+  };
+}
