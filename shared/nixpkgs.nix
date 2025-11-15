8bb7e99ef91053fdf70f6bd8afef24dfa1750e67
NewDawn0 <newdawn.v0.0+git@gmail.com>
2025-11-15 02:24:21 +0100

    Add nix (package manager) config
    Signed-off-by: NewDawn0 <newdawn.v0.0+git@gmail.com>

--- shared/nixpkgs.nix
+++ shared/nixpkgs.nix
@@ -6,5 +6,41 @@
   nixpkgs = {
     config.allowUnfree = true;
     hostPlatform = lib.mkDefault "x86_64-linux";
+    overlays = with args; [
+      boot-club.overlays.default
+    ];
   };
+  nix = {
+    gc.automatic = true;
+    registry = {
+      devShells.flake = args.devShells;
+      unstable.flake = args.nixpkgs-unstable;
+    };
+    settings = {
+      cores = 0;
+      max-jobs = "auto";
+      auto-optimise-store = true;
+      trusted-users = [
+        "root"
+        "@wheel"
+      ];
+      experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
+      substituters = [
+        "https://cache.nixos.org/"
+        "https://nix-community.cachix.org"
+      ];
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
+    };
+  };
+  # TODO: Readd on next nixos stable update
+  # services.angrr = {
+  #   enable = true;
+  #   enableNixGcIntegration = true;
+  #   period = "2weeks";
+  # };
 }
