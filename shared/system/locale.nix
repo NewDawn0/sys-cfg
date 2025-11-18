af34a42b45ddc4640af5edb274e4fc5625791b51
NewDawn0 <newdawn.v0.0+git@gmail.com>
2025-11-18 16:44:15 +0100

    add locale, timezone and keyboard layout

---
+++ shared/system/locale.nix
@@ -0,0 +1,10 @@
+{
+  # Locale & Keymap
+  i18n.defaultLocale = "en_GB.UTF-8";
+  console.keyMap = "sg";
+  time.timeZone = "Europe/Zurich";
+  services.xserver.xkb = {
+    layout = "ch";
+    variant = "de_nodeadkeys";
+  };
+}
